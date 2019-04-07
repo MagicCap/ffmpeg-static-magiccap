fca40bfc469deadfbb8991f07ef6d5577261d233
Jake Gealer <jake@gealer.email>
2019-04-07 11:46:24 +0100

    More stripping of unneeded stuff

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var os = require('os')
 var path = require('path')
 
-var platform = os.platform()
-if (platform !== 'linux' && platform !== 'darwin') {
-  console.error('Unsupported platform.')
+var arch = os.arch()
+if (arch !== 'x64') {
+  console.error('Unsupported architecture.')
   process.exit(1)
 }
 
-var arch = os.arch()
-if (platform === 'darwin' && arch !== 'x64') {
-  console.error('Unsupported architecture.')
+var platform = os.platform()
+if (platform !== 'linux' && platform !== 'darwin') {
+  console.error('Unsupported platform.')
   process.exit(1)
 }
 
